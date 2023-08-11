9986f052ec9381aec05590af4802ff3a37384e0a
ozank <ozankoksal@hotmail.com>
2023-08-11 11:35:57 +0300

    Basic http server

--- wiki.go
+++ wiki.go
@@ -1,4 +1,15 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
+func root(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintf(w, "hello\n")
+}
+
 func main() {
+	http.HandleFunc("/", root)
+	http.ListenAndServe(":8001", nil)
 }
